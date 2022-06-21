b7e9d904318105016cba94a139ffe31eedf29cb9
Christian Frias <39595889+christianfrias@users.noreply.github.com>
2022-06-21 01:01:58 -0400

    Add tailwind and typescript, set up header

---
+++ components/Header.tsx
@@ -0,0 +1,45 @@
+import { MenuAlt1Icon } from "@heroicons/react/solid"
+import Link from "next/link"
+import {useState, useEffect} from "react"
+
+function Header() {
+    const [isScrolled, setIsScrolled] = useState(false)
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 0) {
+                setIsScrolled(true)
+            } else {
+                setIsScrolled(false)
+            }
+        }
+
+        window.addEventListener("scroll", handleScroll)
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll)
+        }
+    },[])
+
+    return (
+        <header className={`${isScrolled && 'bg-red-500'}`}>
+            <div>
+                <Link href="/">
+                    <img src="/images/logos/logo_wh.svg" className="cursor-pointer object-contain" width={52} height={40} />
+                </Link>
+            </div>
+            <div className="flex space-x-4">
+                <ul className="hidden sm:flex space-x-4">
+                    <li className="headerLink">Home</li>
+                    <li className="headerLink">Work</li>
+                    <li className="headerLink">CV</li>
+                    <li className="headerLink">About</li>
+                    <li className="headerLink">Contact</li>
+                </ul>
+                <MenuAlt1Icon className="h-6 w-6" />
+            </div>
+        </header>
+    )
+}
+
+export default Header
---
+++ pages/index.tsx
@@ -0,0 +1,29 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import Header from '../components/Header'
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-row justify-center py-2">
+      <Head>
+        <title>OriginalSpecies by Christian Frias</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header/>
+      <main>
+        {/* Banner */}
+        <section>
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+        </section>
+      </main>
+      {/* Footer */}
+    </div>
+  )
+}
+
+export default Home
